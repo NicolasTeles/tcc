e69c6051906d705a035413a9badb5cd9a1161f87
NicolasTeles <nicolasrmendoncat@gmail.com>
2023-01-16 14:51:03 -0300

    jabaria burro

--- atualizarPedidoProcesso.php
+++ atualizarPedidoProcesso.php
@@ -2,12 +2,12 @@
 header("location: verPedidos.php");
 require_once('conexaoPedido.php');
 echo $_POST['idHidden'];
-$selectUpdate = 'SELECT `img_produto`, `nm_produto`, `qtde_produto`, `desc_produto`, `preco_produto`, `total_produto`, `dataPedido_produto`, `status_pedido`, `id_pedido` FROM `pedido`';
+$selectUpdate = 'SELECT * FROM pedido';
 $selectPedidos = $conn->query($selectUpdate);
 
 while($updatePedidos = $selectPedidos->fetch_assoc()){
         if($updatePedidos['status_pedido'] == 'Pedido em processo'){
-            $updatePendente = 'UPDATE `pedido` SET `status_pedido`="Pedido concluído" WHERE id_pedido='.$_POST['idHidden']; 
+            $updatePendente = 'UPDATE pedido SET `status_pedido`="Pedido concluído" WHERE id_pedido='.$_POST['idHidden']; 
             $conn->query($updatePendente);
         }
 }
